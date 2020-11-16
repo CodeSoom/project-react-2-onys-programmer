4085686da18253086d5ae85761ff124ecec41d70
thkim <thkim2017@gmail.com>
2020-11-16 19:56:02 +0900

    Create a GuidePage component
    It will be an intro of guidepages

---
+++ src/GuidePage.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import GuidePage from './GuidePage';
+
+test('GuidePage', () => {
+
+const { getByText } = render((
+  <GuidePage />
+));
+
+  expect(getByText('Flexible에 오신 것을 환영합니다!')).not.toBeNull();
+  expect(getByText('Flexible은 귀하의 발전에 최상의 assist를 제공하는 utility가 될 것입니다.')).not.toBeNull();
+  expect(getByText('습관 생성 단계')).not.toBeNull();
+});
