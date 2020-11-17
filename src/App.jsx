a70b5a2272d699ec92dead81287d0b2518b1bf80
thkim <thkim2017@gmail.com>
2020-11-17 11:10:12 +0900

    Create and apply NotFoundPage
    to inform user that it is not valid access

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,17 @@ import {
 
 import HomePage from './HomePage';
 import GuidePage from './GuidePage';
+import NotFoundPage from './NotFoundPage';
 
 export default function App() {
 
   return (
-    <>
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/guide" component={GuidePage} />
+          <Route component={NotFoundPage} />
         </Switch>
       </BrowserRouter>
-    </>
   );
 }
---
+++ src/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <>
+      <p>404 not found</p>
+    </>
+  );
+}
