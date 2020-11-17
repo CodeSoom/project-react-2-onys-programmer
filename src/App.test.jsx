fc3f4f5789428bc5c78c4431dc0220799aaeca08
thkim <thkim2017@gmail.com>
2020-11-17 22:23:57 +0900

    Refactor the App.test.jsx
    Create a function renderApp() to Remove duplicated codes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,15 +8,20 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
+function renderApp({ path }) {
+  return(
+    render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ))
+  );
+}
+
 describe('App', () => {
   context('with path /', () => {
     it('renders homepage', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/']}>
-          <App />
-
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/' })
 
       expect(getByText('Flexible')).not.toBeNull();
     });
@@ -24,11 +29,7 @@ describe('App', () => {
 
   context('with path /guide', () => {
     it('renders guidepage', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/guide']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/guide' })
 
       expect(getByText(/환영합니다/)).not.toBeNull();
     });
@@ -36,11 +37,7 @@ describe('App', () => {
 
   context('with path /guide/inputname', () => {
     it('renders input name page', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/guide/inputname']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/guide/inputname'})
 
       expect(getByText(/이름을 입력해주세요/)).not.toBeNull();
     });
@@ -48,11 +45,7 @@ describe('App', () => {
 
   context('with path /guide/inputhabit', () => {
     it('renders input habit page', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/guide/inputhabit']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/guide/inputhabit'})
 
       expect(getByText(/무엇을 습관으로 만들고 싶나요?/)).not.toBeNull();
     });
@@ -60,23 +53,15 @@ describe('App', () => {
 
   context('with path /guide/verticalflexibility', () => {
     it('renders verticalflexibility page', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/guide/verticalflexibility']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/guide/verticalflexibility' })
 
-      expect(getByText(/수직적 유연성/)).not.toBeNull();
+      expect(getByText(/수직적 유연성 확보/)).not.toBeNull();
     });
   });
 
   context('with a path that is not exist', () => {
     it('renders not foundpage', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/anyPathThatIsNotExist']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/anyPathThatIsNotExist' })
 
       expect(getByText(/not found/)).not.toBeNull();
     });
