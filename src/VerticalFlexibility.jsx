07f1a7b460b83502640b7c41570df0918a9a70ce
thkim <thkim2017@gmail.com>
2020-11-17 20:51:44 +0900

    Implement VerticalFlexibility component
    It guides user to expand options of certain habit.

---
+++ src/VerticalFlexibility.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function VerticalFlexibility() {
+  return (
+    <>
+      <h1>매일 운동하기</h1>
+      <h2>수직적 유연성 확보하기!</h2>
+      <label>가장 이상적인 옵션 - Elite<br>
+      </br><input type="text" placeholder="헬스장 가서 스트레칭 후 근력운동 후 유산소운동 후 스트레칭"></input></label>
+      <button>입력</button>
+    </>
+  );
+}
