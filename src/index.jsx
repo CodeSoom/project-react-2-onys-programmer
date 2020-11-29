e0add554127e1222955249d2f7098f4c855dea97
thkim <thkim2017@gmail.com>
2020-11-29 22:56:32 +0900

    Correct basename
    correct basename : remove '/

--- src/index.jsx
+++ src/index.jsx
@@ -14,7 +14,7 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <BrowserRouter basename="/project-react-2-onys-programmer">
+      <BrowserRouter basename="project-react-2-onys-programmer">
         <App />
       </BrowserRouter>
     </Provider>
