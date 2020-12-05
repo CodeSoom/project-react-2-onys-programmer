3e0e3ef8b119d15eacfe82ad9a6cea7a6f89c78f
thkim <thkim2017@gmail.com>
2020-12-05 14:47:31 +0900

    Immigrate CategoryCreateContainer
    From HabitMatrixContainer to HorizontalFlexibility

---
+++ src/HorizontalFlexibility.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import categories from '../fixtures/categories';
+
+import { render } from '@testing-library/react';
+
+import HorizontalFlexibility from './HorizontalFlexibility';
+
+test('HorizontalFlexibility', () => {
+  useSelector.mockImplementation((selector) => selector({
+    categories,
+  }))
+
+  render
+  (
+    <HorizontalFlexibility />
+  );
+});
