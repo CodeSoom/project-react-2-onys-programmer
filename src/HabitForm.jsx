fb896214b99accbaba13b76fd12e90906ec931f5
thkim <thkim2017@gmail.com>
2020-12-14 23:14:46 +0900

    Apply css in HabitForm
    Make componets to be right location plus set margin

--- src/HabitForm.jsx
+++ src/HabitForm.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+
 import GuideMessage from './GuideMessage';
 
 import { Link } from 'react-router-dom';
 
+const Inputs = styled.body({
+  margin: '2% 0 auto',
+  height: '500',
+  width: '300',
+  textAlign: 'center',
+})
+
 export default function HabitForm({ habit, onChange, onClick }) {
-  
+
   function handleChange(event) {
     const { target: { name, value } } = event;
     onChange({ name, value });
@@ -14,28 +23,28 @@ export default function HabitForm({ habit, onChange, onClick }) {
   return (
     <div>
       <GuideMessage />
-      <input
-      type="text"
-      name="userName"
-      placeholder="이름을 입력해주세요!"
-      value={habit.userName}
-      onChange={handleChange}
-      />
-      <input
-      type="text"
-      name="objectHabit"
-      placeholder="목표 습관은 무엇인가요?"
-      value={habit.objectHabit}
-      onChange={handleChange}
-      />
-      <button
-        type="button"
-        onClick={onClick}
-      >
-        <Link to="horizontal-flexibility">
-          입력
-        </Link> 
-    </button>
+      <Inputs>
+        <input
+          type="text"
+          name="userName"
+          placeholder="이름을 입력해주세요!"
+          value={habit.userName}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="objectHabit"
+          placeholder="목표 습관은 무엇인가요?"
+          value={habit.objectHabit}
+          onChange={handleChange}
+        />
+        <button
+          type="button"
+          onClick={onClick}
+        ><Link to="horizontal-flexibility">
+            입력
+          </Link></button>
+      </Inputs>
     </div>
   )
 } 
