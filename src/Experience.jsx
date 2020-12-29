9edd6777b23d56e10525991f670dc29b73389312
thkim <thkim2017@gmail.com>
2020-12-29 23:39:17 +0900

    Change LV logic
    LV depends on experience value

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Experience({ status }) {
   return (
     <>
-      <p>LV.{status.level}</p>
+      <p>LV.{Math.floor(status.experience/7+1)}</p>
       <progress role="experienceBar" value={status.experience} max="7"></progress>
     </>
   );
