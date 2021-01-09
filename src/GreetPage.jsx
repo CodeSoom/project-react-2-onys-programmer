d0c330df3668a38c5010e40182d048af6820169f
thkim <thkim2017@gmail.com>
2021-01-09 16:36:17 +0900

    Add an image It shows the identity of this project

--- src/GreetPage.jsx
+++ src/GreetPage.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import styled from '@emotion/styled';
 
+import controller from './images/controller.jpg';
+
 import { Link } from 'react-router-dom';
 
 const Container = styled.div({
@@ -24,6 +26,13 @@ const Instruction = styled.div({
   textAlign: 'center',
 });
 
+const MainImage = styled.div({
+  margin: '1% 0 auto',
+  height: '100',
+  width: '300',
+  textAlign: 'center',
+})
+
 const Button = styled.div({
   margin: '2% auto',
   Width: '15%',
@@ -33,21 +42,29 @@ const Button = styled.div({
 export default function GreetPage() {
   return (
     <>
-    <Container>
-      <h1>Flexible에 오신 것을 환영합니다!</h1>
-    </Container>
-    <Intro>
-      <p>Flexible은 신개념 습관 형성 프로그램입니다.</p>
-    </Intro>
-    <Instruction>
-      <p>인생이라는 게임에서, <b>습관</b>은 여러분의 강력한 패시브 스킬입니다</p>
-    </Instruction>
-    <Instruction>
-      <p>습관을 만들고 매일 성장시키세요!</p>
-    </Instruction>
-    <Button>
-      <button><Link to="/habit-create">튜토리얼 시작</Link></button>
-    </Button>
+      <Container>
+        <h1>Flexible에 오신 것을 환영합니다!</h1>
+      </Container>
+      <Intro>
+        <p>Flexible은 신개념 습관 형성 프로그램입니다.</p>
+      </Intro>
+      <Instruction>
+        <p>인생이라는 게임에서, <b>습관</b>은 여러분의 강력한 패시브 스킬입니다</p>
+        <MainImage>
+          <img
+            src={controller}
+            width="350"
+          />
+        </MainImage>
+        <p>인생의 컨트롤러는 준비되어있습니다</p>
+      </Instruction>
+
+      <Instruction>
+        <p>습관을 만들고 매일 성장시키세요!</p>
+      </Instruction>
+      <Button>
+        <button><Link to="/habit-create">튜토리얼 시작</Link></button>
+      </Button>
     </>
   );
 }
